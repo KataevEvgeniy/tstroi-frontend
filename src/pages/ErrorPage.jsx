5dd85d06d9e9d4f58da2d9cd090307d0663a3a4c
resh <resh@archlinux.opg>
2023-09-25 19:48:26 +0500

    add error page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -7,9 +7,14 @@ function ErrorPage() {
   let error = useRouteError();
   console.error(error);
   return (
-    <Paper>
-      <Typography>Ой, случилась какая-то ошибка</Typography>
-    </Paper>
+    <div className="flex items-center justify-center flex-col text-stone-700 mt-16">
+      <h1 className="text-3xl font-extrabold">Oops!</h1>
+      <p>Sorry, an unexpected error has occurred.</p>
+      <p>
+        <i>{error.statusText || error.message}</i>
+      </p>
+      <a href="/dashboard">Get back to the main page</a>
+    </div>
   );
 }
 
