7e4d77f5867f2b60c45a2212639ac0b1f4bc643c
resh <resh@archlinux.opg>
2023-09-25 21:12:29 +0500

    hello

--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -1,13 +1,9 @@
-import {
-  Paper,
-  Card,
-  CardMedia,
-  CardContent,
-  Typography,
-  CardActions,
-} from "@mui/material";
 import React, { useEffect, useState } from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
+import { Typography, CardMedia, Stack, Avatar } from "@mui/material";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
@@ -23,32 +19,34 @@ function ProjectsPage() {
   }),
     [projectList];
   return (
-    <Card sx={{ maxWidth: 1592 }}>
-      <CardMedia
-        sx={{ height: 284 }}
-        image="/static/images/1.png"
-        title="construction"
-      />
-      <CardContent>
-        <Typography variant="body2" color="text.secondary">
-          ТСТРОЙ
-        </Typography>
-        <Typography gutterBottom variant="h5" component="div">
-          Проекты
-        </Typography>
-        <Button
-          style={{ marginLeft: "1400px", marginTop: "20px" }}
-          variant="contained"
-          color="success"
-          sx={{ mb: 5 }}
-          onClick={() => {
-            navigate("create");
-          }}
-        >
-          Новый проект
-        </Button>
-      </CardContent>
-    </Card>
+    <>
+      <Card sx={{ maxWidth: 1592 }}>
+        <CardMedia
+          sx={{ height: 284 }}
+          image="/static/images/1.png"
+          title="construction"
+        />
+        <CardContent>
+          <Typography variant="body2" color="text.secondary">
+            ТСТРОЙ
+          </Typography>
+          <Typography gutterBottom variant="h5" component="div">
+            Проекты
+          </Typography>
+          <Button
+            style={{ marginLeft: "1400px", marginTop: "20px" }}
+            variant="contained"
+            color="success"
+            sx={{ mb: 5 }}
+            onClick={() => {
+              navigate("create");
+            }}
+          >
+            Новый проект
+          </Button>
+        </CardContent>
+      </Card>
+    </>
   );
 }
 
